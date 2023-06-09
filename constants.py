50c01278734faeec85face298a348e20b9ce4bc3
marcel8168 <marcel_hiltner@protonmail.com>
2023-06-09 11:39:43 +0200

    [bug] fix error when having no new annotation to add

--- constants.py
+++ constants.py
@@ -1,20 +1,13 @@
 
 SUGGESTION_ANNOTATION_TYPE = "TO_BE_ANNOTATED"
-"""
-PATH_TO_BRAT = "/home/linuxmint/Documents/Brat/brat-1.3p1"
-FOLDER_NAME = "data"
-COLLECTION_NAME = "tutorials/bio"
-"""
-"""
-PATH_TO_BRAT = "/home/linuxmint/Documents/annotationimage"
+
+PATH_TO_BRAT = "."
 FOLDER_NAME = "doc"
 COLLECTION_NAME = None
-"""
-"""
-FILE_NAME = "040-text_span_annotation"
+FILE_NAME = None
 """
 PATH_TO_BRAT = "C:\\Users\\albbl\\Documents\\Studium\\10_Semester"
 FOLDER_NAME = "Projekt"
 COLLECTION_NAME = None
 FILE_NAME = "example"
-
+"""

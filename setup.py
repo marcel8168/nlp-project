bd318c1414b942f7db9e4fdc89932c612fe87548
marcel8168 <marcel_hiltner@protonmail.com>
2023-06-10 11:17:52 +0200

    [feature] add information to setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,32 @@
 from setuptools import setup, find_packages
 
-setup(name='nlp_project', version='1.0', packages=find_packages())
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='nlp_project',
+    version='1.0', 
+    author='Marcel Hiltner',
+    author_email='marcel.hiltner@fau.de',
+    url="https://github.com/marcel8168/nlp-project",
+    packages=find_packages(),
+    description="NLP-Project @FAU",
+    long_description=long_description,
+    install_requires=[
+        datasets==2.12.0,
+        evaluate==0.4.0,
+        modAL==0.4.1,
+        modAL==0.49.2348,
+        nltk==3.8.1,
+        numpy==1.23.5,
+        pandas==2.0.1,
+        scikit_learn==1.2.2,
+        setuptools==65.5.0,
+        setuptools==67.7.2,
+        setuptools==67.8.0,
+        torch==2.0.0,
+        torch==2.0.1,
+        transformers==4.28.1,
+        transformers==4.29.1
+    ]
+)

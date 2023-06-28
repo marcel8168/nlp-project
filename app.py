aafead580efab8f7adccd9e4ff8b807a55cb38fb
marcel8168 <marcel_hiltner@protonmail.com>
2023-06-28 21:58:34 +0200

    [feature] add flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    iframe = 'http://localhost:8001/index.xhtml'
+    return render_template('index.html', iframe=iframe)
+
+@app.route('/annotation_begin')
+def annotation_begin():
+    return render_template('annotation_begin.html')
+
+@app.route('/annotation_end')
+def annotation_end():
+    return render_template('annotation_ned.html')
+
+if __name__ == '__main__':
+    app.run()
